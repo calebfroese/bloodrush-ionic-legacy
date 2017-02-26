e387f564e79f92156e7dc7e6c7961c39665bc69a
Caleb Froese <caleb.froese@gmail.com>
2017-02-26 10:45:12 +1030

    Make the homepage somewhat interesting

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -5,12 +5,19 @@ import {NavController} from 'ionic-angular';
 
 import {AccountService} from '../../app/shared/account.service';
 import {LoginComponent} from '../login/login.component';
+import {Config} from '../../app/shared/config';
+import {environment} from '../../environment/environment';
 
 @Component({selector: 'page-home', templateUrl: 'home.component.html'})
 export class HomeComponent {
+  config = Config;
+  envName = environment.envName;
+
   constructor(
       public navCtrl: NavController, public modalCtrl: ModalController,
       public acc: AccountService, public loadCtrl: LoadingController) {}
+
+
   openLogin(): void {
     let modal = this.modalCtrl.create(LoginComponent);
     modal.present();
